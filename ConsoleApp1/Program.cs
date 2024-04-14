a2d76b678cf78d869049e4d3f9d98a3f5a13cc8e
MehemmedIsmayilov <mahammadmi@code.edu.az>
2024-04-14 14:33:56 +0400

    loops dersde

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,275 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1111111111111111111111111111111111111111111111
+
+            //int num = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if (num >= 0)
+            //{
+            //    Console.WriteLine("Eded Musbetdir");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Eded Menfidir");
+            //}
+
+            //22222222222222222222222222222222222222222222222
+
+            //int toplam = 0;
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine(i+ "Eded qeyd edin");
+            //    int sayi = (int)Convert.ToUInt32(Console.ReadLine());
+            //    toplam += sayi;
+            //}
+            //Console.WriteLine(toplam);
+
+
+
+
+            //333333333333333333333333333333333333333333333333
+
+
+            //int toplam = 0;
+            //for (int i = 0; i < 1; i++)
+            //{
+            //    Console.WriteLine(i + "Eded qeyd edin");
+            //    int sayi = (int)Convert.ToUInt32(Console.ReadLine());
+            //    toplam = sayi*sayi;
+            //}
+            //Console.WriteLine(toplam);
+
+            //4444444444444444444444444444444444444444444444
+
+
+
+
+
+
+            //Console.WriteLine("Eded qeyd edin");
+            //int sayi = (int)Convert.ToUInt32(Console.ReadLine());
+            //int sayi2 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int sayi3 = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if (sayi > sayi2 & sayi>sayi3)
+            //{
+            //    Console.WriteLine("cavab :"+sayi);
+            //}
+            //else if(sayi2>sayi & sayi2>sayi3)
+            //{
+            //    Console.WriteLine("cavab :"+sayi2);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("cavab :"+ sayi3);
+            //}
+
+
+
+
+            //555555555555555555555555555555555555555555
+
+
+
+
+            //int num1 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int num2 = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if (num1 * num1 * num1 > num2)
+            //{
+            //    Console.WriteLine("birinci eded boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ikinci eded boyukdur");
+            //}
+
+
+
+            //66666666666666666666666666666666666666666666
+
+
+            //int num = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if (num%3==0 & num%5==0)
+            //{
+            //    Console.WriteLine("bolunur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("bolunmur");
+            //}
+
+
+            //77777777777777777777777777777777777777777777
+
+
+            //int fen1 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int fen2 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int fen3 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int fen4 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int fen5 = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if ((fen1+fen2+fen3+fen4+fen5)/5 >65 )
+            //{
+            //    Console.WriteLine("Smestri kecdiniz");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Smestrden kesildiniz");
+            //}
+
+
+            //8888888888888888888888888888888888888888888888
+
+
+            //int num1 = (int)Convert.ToUInt32(Console.ReadLine());
+            //int num2 = (int)Convert.ToUInt32(Console.ReadLine());
+
+            //if (num1>num2)
+            //{
+            //    Console.WriteLine("birinci eded boyukdur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ikinci eded boyukdur");
+            //}
+
+
+
+            //99999999999999999999999999999999999999999999999
+
+
+
+            //int SuyunDonmaTEmpraturu =0;
+            //double SuyunTEmpraturu = (int)Convert.ToDouble(Console.ReadLine());
+            //if (SuyunDonmaTEmpraturu>SuyunTEmpraturu)
+            //{
+            //    Console.WriteLine("su donacaq");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("su donmayacaq");
+            //}
+
+
+
+            //1111111111111111111111111111111111111111111111111
+
+
+
+            //for (int i = 0; i <= 50; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+
+            //22222222222222222222222222222222222222222222222
+
+
+            //int sum = 0;
+            //for (int i = 0; i <= 40; i++)
+            //{
+            //    if (i%3==0)
+            //    {
+            //        sum += i;
+
+            //    }
+            //}
+            //Console.WriteLine(sum);
+
+
+            //33333333333333333333333333333333333333333333333
+
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i%3==0 & i%7==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+
+            //44444444444444444444444444444444444444444444
+
+
+            //int num = (int)Convert.ToInt32(Console.ReadLine());
+            //int sayan = 0;
+            //while (0 < num)
+            //{
+
+            //    sayan = num % 10;
+            //    num /= 10;
+            //}
+
+            //Console.WriteLine(sayan);
+
+
+            //55555555555555555555555555555555555555555555
+
+
+
+
+            //int num = (int)Convert.ToInt32(Console.ReadLine());
+            //int say = 0;
+            //while ( num>0)
+            //{
+            //   say=num % 10;
+            //    Console.Write(say);
+            //    num /= 10;
+            //}
+
+
+            //6666666666666666666666666666666666666666666
+            //7777777777777777777777777777777777777777777
+            //8888888888888888888888888888888888888888888
+            //9999999999999999999999999999999999999999999
+
+
+
+            //1000000000000000000000000000000000000000000
+
+
+            //int num = 70;
+
+
+            //for (int i = num; i > 0; i--)
+            //{
+            //    num--;
+            //    if (num % 2==0)
+
+            //    {
+            //        Console.WriteLine(num);
+
+            //    }
+
+            //}
+
+
+
+
+            //11 11 11 11
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                int ulduz = 1;
+                
+                
+                Console.WriteLine(ulduz+ulduz);
+                
+                
+            }
+        }
+    }
+}
